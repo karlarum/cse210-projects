295737405f97e8d0811d7293ed686488f9b09d25
karlarum <karlarummler@gmail.com>
2024-01-16 01:06:11 -0600

    added changes to prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+//using Linq so I can use "max" for step three
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //create a new list with "new" keyword
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+
+        {
+            Console.Write("Enter a number (type 0 when finished): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //compute the sum
+        int sum = 0;
+        //foreach iterates over each element and stores it
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //find max number in list
+        int max = numbers.Max();
+        Console.WriteLine($"The max is: {max}");
+
     }
 }

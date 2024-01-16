10e8020b20beab14f1216c272c35ed9f087c0762
karlarum <karlarummler@gmail.com>
2024-01-15 23:56:20 -0600

    added changes to prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?" );
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // "percent % 10" will get the last number
+        int lastNumber = percent % 10;
+        string sign = "";
+
+         if (lastNumber >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastNumber < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your final grade is {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass.");
+        }
     }
 }

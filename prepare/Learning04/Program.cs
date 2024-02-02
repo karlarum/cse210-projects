850524babb4031c431ac9c588e6e40e2cb0977a3
karlarum <karlarummler@gmail.com>
2024-02-02 01:01:05 -0600

    assignment requirements added

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Create an assignment for base class
+        Assignment assignment1 = new Assignment("John Doe", "C#");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        //Create an assignment for math sub class
+        MathAssignment assignment2 = new MathAssignment("Jane Doe", "Calculus", "3.4", "7-12");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+
+        //Create an assignment for writing sub class
+        WritingAssignment assignment3 = new WritingAssignment("Sherlock Holmes", "Detective 101", "Unsolved Cases");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+        Console.WriteLine();
+
     }
 }
